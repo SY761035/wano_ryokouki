a18c0ee8126cf41e41cc9f8d6c6df8b2b776345e
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-02-05 13:26:18 +0000

    ActiveStorageとコントローラー追加した

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,22 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+  scope module: :public do
+    root to: "homes#top"
+    get "/home/about" => "homes#about", as: "about"
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+    resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
