9ab2da245d230fa0f16eef91b307d3990b891c6a
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-02-27 11:20:12 +0000

    ゲストいいね自動削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  
+
   def reset_guest_data
     guest_user = User.find_by(email: 'guest@example.com')
     guest_user.post_images.destroy_all if guest_user.post_images.any?
     guest_user.post_comments.destroy_all if guest_user.post_comments.any?
+    guest_user.favorites.destroy_all if guest_user.favorites.any?
   end
-  
+
 end
