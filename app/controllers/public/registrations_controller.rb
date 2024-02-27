d00f87e55ca8c6380ffe74fa9314c83512eee756
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-02-27 11:02:36 +0000

    サインアップ後マイページに遷移

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -11,7 +11,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   end
 
   def after_sign_up_path_for(resource)
-    root_path
+    user_path(current_user.id)
   end
 
   # protected
